4cd8d6414ef6e09a0b0438078786bf6b84d44e65
Igor Malyarov <igor1309@gmail.com>
2020-07-05 15:43:56 +0300

    Entity note

--- Sources/InvestmentDataModel/Entity.swift
+++ Sources/InvestmentDataModel/Entity.swift
@@ -10,8 +10,10 @@ import Foundation
 public struct Entity: Identifiable, Equatable, Hashable {
     public var id = UUID()
     public var name: String
+    public var note: String
     
-    public init(_ name: String) {
+    public init(_ name: String, note: String) {
         self.name = name
+        self.note = note
     }
 }
